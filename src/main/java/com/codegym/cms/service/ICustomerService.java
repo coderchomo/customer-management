1263d3fa533c222dc079d5f69a0708fa9d4c72d5
coderchomo <codermermarket@gmail.com>
2021-12-02 21:14:06 +0700

    add search method

--- src/main/java/com/codegym/cms/service/ICustomerService.java
+++ src/main/java/com/codegym/cms/service/ICustomerService.java
@@ -2,7 +2,11 @@ package com.codegym.cms.service;
 
 import com.codegym.cms.model.Customer;
 import com.codegym.cms.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ICustomerService extends IGenericService<Customer> {
     Iterable<Customer> findAllByProvince(Province province);
+    Page<Customer> findAll(Pageable pageable);
+    Page<Customer> findAllByFirstNameContaining(String firstName, Pageable pageable);
 }
